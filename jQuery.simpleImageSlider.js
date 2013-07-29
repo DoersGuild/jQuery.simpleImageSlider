60c661818fc94f839504c8f257fb4d097fa1e9b3
Sathvik Ponangi <sathvik@doersguild.com>
2013-07-29 13:52:28 +0530

    Create jQuery.simpleImageSlider.js

---
+++ jQuery.simpleImageSlider.js
@@ -0,0 +1,21 @@
+(function ($) {
+    "use strict";
+    $.fn.simpleImageSlider(options) {
+        options = $.extend(true, {
+            imageDir: './img/',
+            delayInSeconds: 5,
+            images: [],
+        }, options);
+        return this.each(function () {
+            var $this = $(this);
+            var len = options.images.length;
+            var index = 0;
+            var nextImage = function () {
+                index++;
+                index = index >= len ? 0 : index;
+                $this.attr('src', options.imageDir + options.images[index]);
+            };
+            setInterval(nextImage, options.delayInSeconds * 1000);
+        });
+    }
+})(jQuery);
